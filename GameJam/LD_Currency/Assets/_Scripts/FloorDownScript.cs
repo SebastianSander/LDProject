ecede98f599b00308744658bb85c975aadab23a1
sand4711 <50045177+sand4711@users.noreply.github.com>
2019-04-27 15:50:11 +0200

    add

---
+++ GameJam/LD_Currency/Assets/_Scripts/FloorDownScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorDownScript : MonoBehaviour {
+
+    Collider f_Collider;
+
+    bool sPressed = false;
+
+	void Start () {
+        f_Collider = GetComponent<Collider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            sPressed = true;  
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player"&&sPressed)
+        {
+
+            f_Collider.enabled = !f_Collider.enabled;
+            Debug.Log("Collider.enabled = " + f_Collider.enabled);
+            
+        
+        }
+    }
+}
---
+++ GameJam/LD_Currency/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    [SerializeField]
+    private float speed = 1;
+
+    [SerializeField]
+    private float jumpForce=1;
+    private bool isJumping = false;
+
+    
+
+    Rigidbody player = null;
+
+    
+
+
+
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+
+    }
+	
+	
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0f);
+
+        player.AddForce(movement * speed);
+        
+
+        if (Input.GetKeyDown(KeyCode.Space)&&!isJumping) {
+
+            Vector3 jump = new Vector3(0f, 30f, 0f);
+            player.AddForce(jump * jumpForce);
+            isJumping = true;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Floor"&&isJumping)
+        {
+            isJumping = false;
+            Debug.Log("test");
+        }
+    }
+
+
+}
