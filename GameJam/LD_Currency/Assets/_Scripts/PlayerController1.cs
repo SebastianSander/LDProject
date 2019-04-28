691e575a2632d9ea56c39c36064471f3e68b6ca3
sand4711 <50045177+sand4711@users.noreply.github.com>
2019-04-28 13:45:57 +0200

    Revert "pc"
    This reverts commit 23cfec71c81569ff01ae8d71af3db5d8ef31279e.

--- GameJam/LD_Currency/Assets/_Scripts/PlayerController1.cs
+++ GameJam/LD_Currency/Assets/_Scripts/PlayerController1.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class PlayerController1 : MonoBehaviour {
+
+
+    //private UnityAction deathListener;
+
     
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "InstantTrap")
+    //    {
+    //        //Debug.Log("Event Trigger");
+    //        EventManager.TriggerEvent("instanttrap");
+    //        other.enabled = false;
+    //    }
+
+    //    if (other.gameObject.tag == "DelayTrap")
+    //    {
+    //        //Debug.Log("Event Trigger");
+    //        EventManager.TriggerEvent("delaytrap");
+    //        other.enabled = false;
+    //    }
+
+    //}
+
     
 
 
