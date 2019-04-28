8ef45a5b989f809576c67736bd3dc979ed0695b7
sand4711 <50045177+sand4711@users.noreply.github.com>
2019-04-28 11:42:14 +0200

    Jump, Trap, Button

---
+++ GameJam/LD_Currency/Assets/_Scripts/Traps/ButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+    [SerializeField]
+    GameObject trap;
+
+    private bool ePressed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {   
+
+            ePressed = true;
+        }
+        if (Input.GetKeyUp(KeyCode.E)) {
+            ePressed = false;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player"&&ePressed) {
+            trap.active = false;
+        }
+    }
+}
---
+++ GameJam/LD_Currency/Assets/_Scripts/Traps/TrapScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrapScript : MonoBehaviour {
+
+    private UnityAction deathListener;
+
+    [SerializeField]
+    private bool delay;
+
+    [SerializeField]
+    private float delayTime = 3;
+
+    
+
+    private bool trigger = false;
+
+    private void FixedUpdate()
+    {
+        if (delay)
+        {
+            if (trigger) delayTime -= Time.deltaTime;
+
+            if (delayTime <= 0f)
+            {
+                //Debug.Log("test");
+                EventManager.TriggerEvent("delaytrap");
+                this.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && delay)
+        {
+            Debug.Log("Event Trigger");
+
+            trigger = true;
+
+        }
+        else if (other.gameObject.tag == "Player") {
+            EventManager.TriggerEvent("instanttrap");
+            this.enabled = false;
+        }
+
+
+    }
+
+}
